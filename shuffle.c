54d29a23b177fee8fa0cac736eda04449aa438f3
enkyuan <yuan_enkang@outlook.com>
2026-03-08 20:37:21 -0400

    feat: riffle shuffle

--- shuffle.c
+++ shuffle.c
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Forward declaration so interleaveHelper knows shuffle exists
+void shuffle(CardDeck origDeck, int round);
+
 // do NOT modify this function
 static void printDeck(CardDeck deck)
 {
@@ -18,17 +21,6 @@ static void printDeck(CardDeck deck)
   printf("\n");
 }
 
-#ifdef TEST_DIVIDE
-// leftDeck and rightDeck are arrays of CardDeck
-// This function creates pairs of left and right decks
-// Each pair divides the original deck into two non-overlapping decks and
-// together they form the original deck.
-//
-// You can think of the left deck held by the left hand taking some
-// cards (at least one) from the top of the original deck.
-//
-// The right deck is held by the right hand taking some (at least one)
-// cards from the bottom of the original deck.
 void divide(CardDeck origDeck, CardDeck * leftDeck, CardDeck * rightDeck)
 {
     int origSize = origDeck.size;
@@ -53,51 +45,8 @@ void divide(CardDeck origDeck, CardDeck * leftDeck, CardDeck * rightDeck)
         }
     }
 }
-#endif
 
-#ifdef TEST_INTERLEAVE
-
-// Interleave two decks to generate all possible results.
-//
-// If the leftDeck is {'A'} and the right deck is {'2', '3'}, this
-// function prints
-// A 2 3
-// 2 A 3
-// 2 3 A
-//
-// If the leftDeck is {'A', '2'} and the right deck is {'3', '4'}, this
-// function prints 
-// 3 4 A 2
-// 3 A 4 2
-// A 3 4 2 
-// 3 A 2 4 
-// A 3 2 4 
-// A 2 3 4 
-//
-// Please notice the space does not matter because grading will use
-// diff -w
-//
-// How to generate all possible interleaves?
-
-// Understand that a card at a particular position can come from
-// either left or right (two options). The following uses left for
-// explanation but it is equally applicable to the right.
-//
-// After taking one card from the left deck, the left deck has one
-// fewer card. Now, the problem is the same as the earlier problem
-// (thus, this problem can be solved by using recursion), excecpt one
-// left card has been taken. Again, the next card can come from left
-// or right.
-//
-// This process continues until either the left deck or the right deck
-// runs out of cards. The remaining cards are added to the result.
-// 
-// It is very likely that you want to create a "helper" function that
-// can keep track of some more arguments.  If you create a helper
-// function, please keep it inside #ifdef TEST_INTERLEAVE and #endif
-// so that the function can be removed for grading other parts of the
-// program.
-void interleaveHelper(CardDeck leftDeck, CardDeck rightDeck, CardDeck resultDeck, int leftIndex, int rightIndex, int resultIndex) {
+void interleaveHelper(CardDeck leftDeck, CardDeck rightDeck, CardDeck resultDeck, int leftIndex, int rightIndex, int resultIndex, int round) {
     if (leftIndex == leftDeck.size) 
     {
         for (int i = rightIndex; i < rightDeck.size; i++) 
@@ -105,8 +54,13 @@ void interleaveHelper(CardDeck leftDeck, CardDeck rightDeck, CardDeck resultDeck
             resultDeck.cards[resultIndex] = rightDeck.cards[i];
             resultIndex++;
         }
+        
+        if (round == 1) {
+            printDeck(resultDeck);
+        } else {
+            shuffle(resultDeck, round - 1);
+        }
 
-        printDeck(resultDeck);
         return;    
     } 
     else if (rightIndex == rightDeck.size) 
@@ -117,42 +71,32 @@ void interleaveHelper(CardDeck leftDeck, CardDeck rightDeck, CardDeck resultDeck
             resultIndex++; 
         }
 
-        printDeck(resultDeck);
+        if (round == 1) {
+            printDeck(resultDeck);
+        } else {
+            shuffle(resultDeck, round - 1);
+        }
+
         return;
     }
     else {
         resultDeck.cards[resultIndex] = leftDeck.cards[leftIndex];
-        interleaveHelper(leftDeck, rightDeck, resultDeck, leftIndex + 1, rightIndex, resultIndex + 1);
+        interleaveHelper(leftDeck, rightDeck, resultDeck, leftIndex + 1, rightIndex, resultIndex + 1, round);
 
         resultDeck.cards[resultIndex] = rightDeck.cards[rightIndex];
-        interleaveHelper(leftDeck, rightDeck, resultDeck, leftIndex, rightIndex + 1, resultIndex + 1);
+        interleaveHelper(leftDeck, rightDeck, resultDeck, leftIndex, rightIndex + 1, resultIndex + 1, round);
     }        
 }
 
-void interleave(CardDeck leftDeck, CardDeck rightDeck)
+void interleave(CardDeck leftDeck, CardDeck rightDeck, int round)
 {
     CardDeck resultDeck;
 
     resultDeck.size = leftDeck.size + rightDeck.size;
-    interleaveHelper(leftDeck, rightDeck, resultDeck, 0, 0, 0);
+    interleaveHelper(leftDeck, rightDeck, resultDeck, 0, 0, 0, round);
 }
-#endif
 
-#ifdef TEST_SHUFFLE
-// The shuffle function has the following steps:
-
-// 1. calculate the number of possible left and right decks. It is
-// the number of cards - 1 because the left deck may have 1, 2,...,
-// #cards - 1 cards.
-//
-// 2. allocate memory to store these possible pairs of left and right
-// decks.
-//
-// 3. send each pair to the interleave function
-//
-// 4. release allocated memory
-//
-void shuffle(CardDeck origDeck)
+void shuffle(CardDeck origDeck, int round)
 {
     int numPairs = origDeck.size - 1;
 
@@ -161,12 +105,10 @@ void shuffle(CardDeck origDeck)
 
     divide(origDeck, leftDeck, rightDeck);
 
-    for (int i = 0; i < numPairs; i++) 
-    {
-        interleave(leftDeck[i], rightDeck[i]);
+    for (int i = 0; i < numPairs; i++) {
+        interleave(leftDeck[i], rightDeck[i], round);
     }
 
     free(leftDeck);
     free(rightDeck);
 }
-#endif
